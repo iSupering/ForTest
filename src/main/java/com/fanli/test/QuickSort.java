5bdf685fa9c991030e6caef3b517645ff27de4d2
iSupering <1044343494@qq.com>
2016-07-22 14:14:14 +0800

    添加一个快速排序实现，

--- src/main/java/com/fanli/test/QuickSort.java
+++ src/main/java/com/fanli/test/QuickSort.java
@@ -8,14 +8,14 @@ public class QuickSort {
     public static void main(String[] args) {
 //        int array[] = new int[]{1, 4, 234, 12, 35, 32, 5, 12};
 //        int array[] = new int[]{1, 4, 234, 12, 35, 32, 5, 12};
-        int array[] = new int[]{3,3,3,3,5,5,5,5,4,4,4,4,0};
-        sayHello(array, 0, array.length - 1);
+        int array[] = new int[]{3, 3, 3, 3, 5, 5, 5, 5, 4, 4, 4, 4, 0};
+        quickSort2(array, 0, array.length - 1);
         for (int i : array) {
             System.out.println(i);
         }
     }
 
-    public static void sayHello(int[] array, int sIndex, int fIndex) {
+    public static void quickSort(int[] array, int sIndex, int fIndex) {
         if (sIndex >= fIndex || sIndex < 0 || fIndex >= array.length) {
             return;
         }
@@ -23,15 +23,15 @@ public class QuickSort {
         int j = fIndex;
         int key = array[sIndex];
         while (i < j) {
-            while (array[i] < key)  {
+            while (array[i] < key) {
                 i++;
-                if( i > fIndex){
+                if (i > fIndex) {
                     break;
                 }
             }
-            while (array[j] > key ){
+            while (array[j] > key) {
                 j--;
-                if(j < sIndex){
+                if (j < sIndex) {
                     break;
                 }
             }
@@ -43,7 +43,31 @@ public class QuickSort {
                 j--;
             }
         }
-        sayHello(array, sIndex, i-1);
-        sayHello(array, j+1, fIndex);
+        quickSort(array, sIndex, i - 1);
+        quickSort(array, j + 1, fIndex);
+    }
+
+    public static void quickSort2(int[] array, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int i = left;
+        int j = right;
+        int tmp = array[left];
+        while (i != j) {
+             while (array[j] >= tmp && i < j) j--;
+            while (array[i] <= tmp && i < j) i++;
+
+            if (i < j) {
+                array[i] = array[i] + array[j];
+                array[j] = array[i] - array[j];
+                array[i] = array[i] - array[j];
+            }
+        }
+        array[left] = array[i];
+        array[i] = tmp;
+        quickSort2(array, left, i - 1);
+        quickSort2(array, i + 1, right);
     }
 }
