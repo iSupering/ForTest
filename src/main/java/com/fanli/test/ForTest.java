8de2068d3eb9ca918887cf63e5d5d58ce8fd4670
suing <suSupering@163.com>
2017-05-14 01:28:46 +0800

    提交 测试 一下git这个东西

--- src/main/java/com/fanli/test/ForTest.java
+++ src/main/java/com/fanli/test/ForTest.java
@@ -19,7 +19,6 @@ public class ForTest {
 //            System.out.println("this is a new Test haha");
         }
         //我感觉 我真的很喜欢 骂人呀，哇哈哈
-        int s ;
         System.out.println("我是一只小小鸟，怎么飞也飞不高");
 
     }
